656d6e8243b40af4a6190b57903c1eb6f32b8d66
Joris Pinkse <pinkse@gmail.com>
2020-11-29 23:07:31 -0500

    deleted line

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,5 @@
 
-
-#~ push!(LOAD_PATH,"../src/")
 using Documenter, Bump
-
-#~ println( names(Bump; imported=true) )
-
-#~ makedocs(
-#~     modules=[Bump],
-#~     authors="Joris Pinkse",
-#~     repo="https://github.com/NittanyLion/Bump.jl/blob/{commit}{path}#L{line}",
-#~     sitename="Bump.jl",
-#~     format=Documenter.HTML(;
-#~         prettyurls=get(ENV, "CI", "false") == "true",
-#~         canonical="https://NittanyLion.github.io/Bump.jl",
-#~         assets=String[],
-#~     ),
-#~     pages=[
-#~         "Home" => "index.md",
-#~     ],
-#~ )
-#~ makedocs(sitename="My Documentation")
 
 
 
@@ -32,7 +12,6 @@ makedocs(
     modules = [Bump],
     sitename = "Bump.jl",
     authors = "Joris Pinkse",
-    # linkcheck = true,
     format = format,
     pages = [
         "Home" => "index.md"
@@ -44,5 +23,3 @@ deploydocs(
     repo="github.com/NittanyLion/Bump.jl.git"
 )
 
-
-# **************  REMEMBER NOT TO PUT IN BLANK LINES BETWEEN DOCSTRING AND DEFINITIONS
