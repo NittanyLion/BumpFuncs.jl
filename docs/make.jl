c8c97dd23e44311923b1ff12753b761c15c320b6
Joris Pinkse <pinkse@gmail.com>
2020-11-29 22:18:39 -0500

    ugh

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,45 @@
 #~ push!(LOAD_PATH,"../src/")
 using Documenter, Bump
 
-println( names(Bump; imported=true) )
+#~ println( names(Bump; imported=true) )
+
+#~ makedocs(
+#~     modules=[Bump],
+#~     authors="Joris Pinkse",
+#~     repo="https://github.com/NittanyLion/Bump.jl/blob/{commit}{path}#L{line}",
+#~     sitename="Bump.jl",
+#~     format=Documenter.HTML(;
+#~         prettyurls=get(ENV, "CI", "false") == "true",
+#~         canonical="https://NittanyLion.github.io/Bump.jl",
+#~         assets=String[],
+#~     ),
+#~     pages=[
+#~         "Home" => "index.md",
+#~     ],
+#~ )
+#~ makedocs(sitename="My Documentation")
+
+
+
+format = Documenter.HTML(edit_link = "master",
+                         prettyurls = get(ENV, "CI", nothing) == "true",
+                         assets = [
+                            String[]
+                        ]
+)
 
 makedocs(
-    modules=[Bump],
-    authors="Joris Pinkse",
-    repo="https://github.com/NittanyLion/Bump.jl/blob/{commit}{path}#L{line}",
-    sitename="Bump.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://NittanyLion.github.io/Bump.jl",
-        assets=String[],
-    ),
-    pages=[
+    modules = [Bump],
+    sitename = "Bump.jl",
+    authors = "Joris Pinkse",
+    # linkcheck = true,
+    format = format,
+    pages = [
         "Home" => "index.md",
-    ],
+        "LICENSE.md",
+    ]
 )
-#~ makedocs(sitename="My Documentation")
+
 
 deploydocs(
     repo="github.com/NittanyLion/Bump.jl.git"
