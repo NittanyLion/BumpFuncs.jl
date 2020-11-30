675dd433bab77f851677f885bec7d365eaba396f
Joris Pinkse <pinkse@gmail.com>
2020-11-29 21:14:51 -0500

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,26 @@
 push!(LOAD_PATH,"../src/")
-using Bump
-using Documenter
+using Documenter, Bump
 
-makedocs(;
-    modules=[Bump],
-    authors="Joris Pinkse",
-    repo="https://github.com/NittanyLion/Bump.jl/blob/{commit}{path}#L{line}",
-    sitename="Bump.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://NittanyLion.github.io/Bump.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+println( names(Bump; imported=true) )
+
+#~ makedocs(;
+#~     modules=[Bump],
+#~     authors="Joris Pinkse",
+#~     repo="https://github.com/NittanyLion/Bump.jl/blob/{commit}{path}#L{line}",
+#~     sitename="Bump.jl",
+#~     format=Documenter.HTML(;
+#~         prettyurls=get(ENV, "CI", "false") == "true",
+#~         canonical="https://NittanyLion.github.io/Bump.jl",
+#~         assets=String[],
+#~     ),
+#~     pages=[
+#~         "Home" => "index.md",
+#~     ],
+#~ )
+makedocs(sitename="My Documentation")
 
 deploydocs(;
-    repo="github.com/NittanyLion/Bump.jl",
+    repo="github.com/NittanyLion/Bump.jl.git",
+    target="build",
+    push_preview=true
 )
