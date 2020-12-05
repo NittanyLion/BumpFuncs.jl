bc1c9304672afd502ad6fbe4a94883ff25bc511d
Joris Pinkse <pinkse@gmail.com>
2020-12-04 22:08:51 -0500

    more tests

--- src/try.jl
+++
@@ -1,8 +0,0 @@
-#include("Bump.jl")
-push!(LOAD_PATH, pwd() )
-
-using Bump
-
-println( bump(0.0) )
-println( ddbump(0.1) )
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,10 @@ using Test
     @test bump(1.0) == 0.0
     @test dbump(0.5) ≈ -0.46861713442795866
     @test ddbump(0.1) ≈ -0.758014676692494
+    @test dbump(0.5,0.0,1.0) ≈ dbump(0.5)
+    @test ddbump(0.5,0.0,2.0) ≈ ddbump(0.5) / 4.0
+    @test dbump([0.5],[0.2],[2.0]) ≈ dbump(0.5,0.2,2.0)
+    @test ddbump([0.5],[0.2],[2.0]) ≈ ddbump(0.5,0.2,2.0)
+    @test dbump([0.3]) ≈ dbump(0.3)
+    @test ddbump([0.3]) ≈ ddbump(0.3)
 end
