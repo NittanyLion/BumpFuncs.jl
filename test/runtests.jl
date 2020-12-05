1fdfd439efcc7897f3f24596e3d0227c6e2dd428
Joris Pinkse <pinkse@gmail.com>
2020-12-04 22:15:23 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,8 +11,8 @@ using Test
     @test ddbump(0.1) ≈ -0.758014676692494
     @test dbump(0.5,0.0,1.0) ≈ dbump(0.5)
     @test ddbump(0.5,0.0,1.0) ≈ ddbump(0.5) 
-    @test dbump([0.5],[0.2],[2.0]) ≈ dbump(0.5,0.2,2.0)
-    @test ddbump([0.5],[0.2],[2.0]) ≈ ddbump(0.5,0.2,2.0)
-    @test dbump([0.3]) ≈ dbump(0.3)
-    @test ddbump([0.3]) ≈ ddbump(0.3)
+    @test dbump([0.5],[0.2],[2.0])[1] ≈ dbump(0.5,0.2,2.0)
+    @test ddbump([0.5],[0.2],[2.0])[1,1] ≈ ddbump(0.5,0.2,2.0)
+    @test dbump([0.3])[1] ≈ dbump(0.3)
+    @test ddbump([0.3])[1,1] ≈ ddbump(0.3)
 end
